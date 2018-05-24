68aeb849a96d91bca2ce26070c195b41b6b553dc
karolinemj <karoline_MJ@hotmail.com>
2018-05-24 12:23:11 +0200

    menubar Mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 let burgermenu = document.querySelector(".burgerbutton");
 let navbarBig = document.querySelector(".navbarBig");
 
+let burgermenuMob = document.querySelector(".burgerbuttonMob");
+
+
+burgermenuMob.addEventListener('click', openMenu);
 burgermenu.addEventListener('click', openMenu);
 
 
 function openMenu(){
+    burgermenuMob.classList.toggle('active');
     burgermenu.classList.toggle('active');
     console.log("something happened");
     navbarBig.classList.toggle('enabled');
